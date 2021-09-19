3b93f34d1fd66b58fe518b6f12a7dd39aba4a14e
sszuev <sss.zuev@gmail.com>
2021-09-19 15:19:11 +0300

    tasks: add CharSort generic interface + impl

--- algorithms/src/main/java/com/gitlab/sszuev/tasks/sorting/JDKDualPivotQuickSortAlgorithm.java
+++ algorithms/src/main/java/com/gitlab/sszuev/tasks/sorting/JDKDualPivotQuickSortAlgorithm.java
@@ -6,9 +6,14 @@ import java.util.Arrays;
  * A quick sort from JDK.
  * Created by @ssz on 05.09.2021.
  */
-public class JDKDualPivotQuickSortAlgorithm extends BaseIntegerSortAlgorithm {
+public class JDKDualPivotQuickSortAlgorithm extends BaseIntegerSortAlgorithm implements IntSort, CharSort {
     @Override
     public void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    @Override
+    public void sort(char[] array) {
         Arrays.sort(array);
     }
 }
